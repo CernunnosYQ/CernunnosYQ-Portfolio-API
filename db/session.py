5b1adcf3b76f76301d0b2ef09ba00f387d6bd6a5
Yair Quintana <yairquintanamx@gmail.com>
2024-03-06 04:39:01 -0600

    Create generator for database dependecy injection

--- db/session.py
+++ db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from core.config import settings
 
@@ -8,3 +9,11 @@ DB_URL = settings.DB_URL
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
