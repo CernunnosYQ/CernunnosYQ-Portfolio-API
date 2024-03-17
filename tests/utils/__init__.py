2f9af73c2a9a8417a685e34a4510ffe578a9b486
Yair Quintana <yairquintanamx@gmail.com>
2024-03-16 18:56:22 -0600

    feat(tests): Add utility to create new user for tests

---
+++ tests/utils/__init__.py
@@ -0,0 +1,1 @@
+from .user import create_custom_test_user, create_default_test_user
---
+++ tests/utils/user.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+
+from db.models.user import create_user
+from core.jwt_utils import create_access_token
+from schemas import UserCreate
+
+DEFAULT_DATA = {
+    "username": "testuser",
+    "email": "test@example.com",
+    "password": "Password123!",
+    "password2": "Password123!",
+}
+
+
+def create_custom_test_user(user_data: dict, db: Session):
+    """
+    Creates a test user in the database and returs its ID and authentication token.
+    """
+
+    user = create_user(user=UserCreate(**user_data), db=db)
+
+    access_token = create_access_token(data={"sub": user.username})
+
+    return user.id, access_token
+
+
+def create_default_test_user(db: Session):
+    """
+    Creates a default test user and returns its ID and authentication token.
+    """
+
+    return create_custom_test_user(user=DEFAULT_DATA, db=db)
