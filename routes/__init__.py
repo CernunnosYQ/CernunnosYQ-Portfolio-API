dbb963a2a1a69344eb3b1a954707ad9872f82bed
Yair Quintana <yairquintanamx@gmail.com>
2024-03-16 23:04:59 -0600

    fix(auth): Fix get current user and add auth routes to api router

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .user import user_router
 from .blogpost import blog_router
+from .auth import auth_router
 
 
 api_router = APIRouter()
 api_router.include_router(user_router, prefix="", tags=["users"])
 api_router.include_router(blog_router, prefix="", tags=["blogposts"])
+api_router.include_router(auth_router, prefix="", tags=["authentication"])
--- routes/auth.py
+++ routes/auth.py
@@ -53,10 +53,14 @@ def get_current_user(
     Get current logged-in user
     """
 
-    success, payload = validate_access_token(token)
-    if not success:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=payload)
-    user = retrieve_user_by_username(username=payload["sub"], db=db)
+    validation = validate_access_token(token)
+    if not validation.get("success"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=validation.get("detail")
+        )
+    user = retrieve_user_by_username(
+        username=validation.get("payload").get("sub"), db=db
+    )
     if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
